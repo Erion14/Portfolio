0385e434c43c0e478375b0b7477242e1a458b31a
Erion Ademi <117570706+Erion14@users.noreply.github.com>
2024-01-31 15:30:25 +0100

    Added Navbar logos and added css

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,11 @@ import LogoSub from "../../assets/images/a-s.png";
 import { Link, NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faDiscord,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -32,6 +37,27 @@ const Sidebar = () => (
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"></FontAwesomeIcon>
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/erion-ademi-b730a1230/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://github.com/Erion14">
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="discord:rinkhalz">
+          <FontAwesomeIcon icon={faDiscord} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 );
 
