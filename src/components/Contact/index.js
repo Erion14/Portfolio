a5f7c93b8e3280ebd2cb64d29304b04281abbaa2
Erion Ademi <117570706+Erion14@users.noreply.github.com>
2024-02-03 00:10:47 +0100

    Remove unnecessary code for reloading page after successful message sending

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -29,7 +29,6 @@ const Contact = () => {
       .then(
         () => {
           alert("Message successfully sent");
-          window.location.reload(false);
         },
         () => {
           alert("Failed to send the message, please try again");
