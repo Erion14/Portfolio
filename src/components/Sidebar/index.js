a435c4d84bfa91c7b70a355a99b5bb6d9625efb7
Erion Ademi <117570706+Erion14@users.noreply.github.com>
2024-02-10 22:05:11 +0100

    Refactor Sidebar component to add mobile navigation functionality

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,71 +5,83 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faEnvelope,
   faHome,
-  faSuitcase,
   faUser,
+  faBars,
+  faClose,
 } from "@fortawesome/free-solid-svg-icons";
 import {
   faDiscord,
   faGithub,
   faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
+import React, { useState } from "react";
 
-const Sidebar = () => (
-  <div className="nav-bar">
-    <Link className="logo" to="/">
-      <img src={LogoS} alt="logo"></img>
-    </Link>
-    <nav>
-      <NavLink exact="true" activeclassname="active" to="/">
-        <FontAwesomeIcon icon={faHome} color="#4d4d4e"></FontAwesomeIcon>
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="about-link"
-        to="/about"
-      >
-        <FontAwesomeIcon icon={faUser} color="#4d4d4e"></FontAwesomeIcon>
-        {/* </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="portfolio-link"
-        to="/portfolio"
-      >
-        <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e"></FontAwesomeIcon> */}
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="contact-link"
-        to="/contact"
-      >
-        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"></FontAwesomeIcon>
-      </NavLink>
-    </nav>
-    <ul>
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.linkedin.com/in/erion-ademi-b730a1230/"
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/" onClick={() => setShowNav(false)}>
+        <img src={LogoS} alt="logo" />
+      </Link>
+      <nav className={showNav ? "mobile-show" : ""}>
+        <NavLink exact activeClassName="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact
+          activeClassName="active"
+          className="about-link"
+          to="/about"
         >
-          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-        </a>
-      </li>
-      <li>
-        <a target="_blank" rel="noreferrer" href="https://github.com/Erion14">
-          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-        </a>
-      </li>
-      <li>
-        <a target="_blank" rel="noreferrer" href="discord:rinkhalz">
-          <FontAwesomeIcon icon={faDiscord} color="#4d4d4e" />
-        </a>
-      </li>
-    </ul>
-  </div>
-);
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact
+          activeClassName="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <FontAwesomeIcon
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#9417ba"
+          size="3x"
+          className="close-icon"
+        />
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/erion-ademi-b730a1230/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://github.com/Erion14">
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreferrer" href="discord:rinkhalz">
+            <FontAwesomeIcon icon={faDiscord} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#9417ba"
+        size="3x"
+        className="hamburger-icon"
+      />
+    </div>
+  );
+};
 
 export default Sidebar;
